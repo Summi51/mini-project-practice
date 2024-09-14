fd3b6d457f28c061a6cdc457e721655360e095e9
Samreen Inayat <73363392+Summi51@users.noreply.github.com>
2024-09-14 04:54:04 +0000

    connect to db

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-console.log("hi")
+const express = require("express")
+const {connection }= require("./db")
+require("dotenv").config()
+const app = express()
 
-const x = 33;
-if(x===33){
-    console.log(x,"yes")
-}else{
-  console.log(x, "no")
+app.use(express.json())
+
+app.use('/',(req, res)=>{
+    console.log("Hello world")
+    res.send('Hello macbook')
+})
+
+app.listen(process.env.PORT, async()=>{
+try{
+ await connection
+console.log(`DB is connected`)
+}catch(err){
+console.log(err)
+console.log('DB is not connect')
 }
+console.log(`server is connect on port ${process.env.PORT}`)
+})

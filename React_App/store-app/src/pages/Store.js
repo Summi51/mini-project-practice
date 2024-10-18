140b7b0ff6ae036496b815c4d256df5fb6259845
Summi51 <inayatsamreen1411@gmail.com>
2024-10-18 23:39:29 +0530

    done

--- React_App/store-app/src/pages/Store.js
+++ React_App/store-app/src/pages/Store.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-
 function Store() {
  
   return (
     <div>
       <h1>Store</h1>
       <p>This is a protected page, only accessible after login.</p>
-      {/* Render store data */}
     </div>
   );
 }

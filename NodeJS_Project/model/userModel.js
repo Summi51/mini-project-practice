67d5367d3bc5a8ee92479486b0308831623ee14e
Samreen Inayat <samreeninayat@Samreens-MacBook-Air.local>
2024-10-18 21:30:18 +0530

    backend done

--- NodeJS_Project/model/userModel.js
+++ NodeJS_Project/model/userModel.js
@@ -4,7 +4,8 @@ const userSchema = mongoose.Schema({
     name:{type: String, require:true},
     gender : {type: String, required: true},
     email:{type: String, required:true},
-    password: {type:String, required: true}
+    password: {type:String, required: true},
+    isAdmin: { type: String, default: "false" }
 },{
     versionKey: false
 })
--- NodeJS_Project/routes/userRoutes.js
+++ NodeJS_Project/routes/userRoutes.js
@@ -8,19 +8,20 @@ const bcrypt = require("bcrypt")
 //register
 
 userRoutes.post('/register', async(req, res)=>{
-    const {name, gender, email, password} = req.body   
+    const {name, gender, email, password , isAdmin} = req.body   
     console.log(email)
     console.log(gender)
     console.log(password)
     console.log(name)
+    console.log(isAdmin)
 
     try{
         bcrypt.hash(password, 5, async(err, hash)=>{
-            const user = new UserModel({name, gender, email, password: hash})
+            const user = new UserModel({name, gender, email, password: hash , isAdmin})
             await user.save();
-            res.status(200).send({massege:"New User Ragistered"})
+            res.status(200).send({massege:"New User Ragistered" , isAdmin:isAdmin})
             })
-
+ 
     }catch(error){
         res.status(400).send({err: err.massege})
     }
@@ -32,14 +33,14 @@ userRoutes.post('/login', async(req, res)=>{
 
  try{
     const user = await UserModel.findOne({email})
-    console.log(user, "userData") // email match
+    console.log(user, "userData") 
    
    if(user){
-    bcrypt.compare(password, user.password,(err, result)=>{   // password match
+    bcrypt.compare(password, user.password,(err, result)=>{   
    if(result){
    const token = jwt.sign({authorId: user._id, author: user.name},"storeData")
    console.log(token, "token")
-   res.status(200).send({msg:"User login Successfully",token:token} )
+   res.status(200).send({msg:"User login Successfully",token:token , isAdmin:isAdmin} )
    }else{
        res.status(400).send({msg:"User Not Login"})
    }
